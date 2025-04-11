52b972a825465beebc4da8430baa2bb8ee6e6ee7
Maxime Hardy <maxime.bonneau.hardy@gmail.com>
2025-04-11 13:42:44 -0400

    Fix: Algorithme des abeilles

--- abeilles/create_video.sh
+++ abeilles/create_video.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -framerate 2 -start_number 1 -i algorithme/cycle_%d.png -c:v libopenh264 -pix_fmt yuv420p algorithme.mp4
+ffmpeg -framerate 1 -start_number 1 -i algorithme/cycle_%d.png -c:v libopenh264 -pix_fmt yuv420p algorithme.mp4
